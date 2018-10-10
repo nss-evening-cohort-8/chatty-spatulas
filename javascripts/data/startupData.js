d205966b56ac4afcbb861b020cfdaca156fa00aa
Lance Hamilton <lance.hamilton24@gmail.com>
2018-10-09 20:16:11 -0500

    added xhr code

--- javascripts/data/startupData.js
+++ javascripts/data/startupData.js
@@ -1,1 +1,21 @@
-console.log('startupData');
+function getData () {
+    const data = JSON.parse(this.responseText);
+}
+
+function ifDataFails () {
+    console.log('Xhr failed');
+
+}
+
+
+
+
+const getMessages = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener('load', getData);
+    myRequest.addEventListener('error', executeThisCodeifXhrFails);
+    myRequest.open('GET', './db/pets.json');
+    myRequest.send();
+};
+
+export {getMessages};
