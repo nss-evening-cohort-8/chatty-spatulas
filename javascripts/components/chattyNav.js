9083f04ebcb713b6f41576e5518963b4d14dc961
mohammad <khezri@outlook.com>
2018-10-10 20:22:29 -0500

    Finished the dropdown menu

--- javascripts/components/chattyNav.js
+++ javascripts/components/chattyNav.js
@@ -1,6 +1,19 @@
-const users = ['Mohammad', 'Lance', 'Marco', 'Maggie'];
-      const select = document.getElementById('users');
+import {printToDom} from "../helpers/util.js";
 
-for (name in users){
-    select.add (new Option(users[name]));
-};
+const users = ['Mohammad', 'Lance', 'Marco', 'Maggie'];
+
+      const userPopulate = () => {
+        let domString = '';
+
+          domString += `<button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown"> Select User </button>`;    
+            domString += `<div class="dropdown-menu">`;
+                domString += `<a class="d-inline dropdown-item" href="#" id="Moh-user">${users[0]}</a>`;
+                domString += `<a class="dropdown-item" href="#" id="Lan-user">${users[1]}</a>`;
+                domString += `<a class="dropdown-item" href="#" id="Mar-user">${users[2]}</a>`;
+                domString += `<a class="dropdown-item" href="#" id="Mag-user">${users[3]}</a>`;
+            domString += `</div>`;
+
+            printToDom(domString, "users-select");
+    };
+    
+export {userPopulate}
