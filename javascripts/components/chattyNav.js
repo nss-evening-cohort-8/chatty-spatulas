95743d7e027d6cb98a443f46cbdd31391b169813
mohammad <khezri@outlook.com>
2018-10-10 23:57:02 -0500

    Fixed the dropdown

--- javascripts/components/chattyNav.js
+++ javascripts/components/chattyNav.js
@@ -1,19 +1,17 @@
-import {printToDom} from "../helpers/util.js";
+import { printToDom } from "../helpers/util.js";
 
 const users = ['Mohammad', 'Lance', 'Marco', 'Maggie'];
 
-      const userPopulate = () => {
-        let domString = '';
+const userPopulate = () => {
+  let domString = '';
+  domString += `<select class="selectpicker btn-success">`;
+  domString += `<option class="FirstItemHide">Select User</option>`;
+  for (let i = 0; i < users.length; i++) {
+    domString += `<option class="bg-light text-dark my-4" id="${users[i]}-user">${users[i]}</option>`;
+  };
+  domString += `</select>`;
 
-          domString += `<button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown"> Select User </button>`;    
-            domString += `<div class="dropdown-menu">`;
-                domString += `<a class="d-inline dropdown-item" href="#" id="Moh-user">${users[0]}</a>`;
-                domString += `<a class="dropdown-item" href="#" id="Lan-user">${users[1]}</a>`;
-                domString += `<a class="dropdown-item" href="#" id="Mar-user">${users[2]}</a>`;
-                domString += `<a class="dropdown-item" href="#" id="Mag-user">${users[3]}</a>`;
-            domString += `</div>`;
+  printToDom(domString, "users-select");
+};
 
-            printToDom(domString, "users-select");
-    };
-    
-export {userPopulate}
+export { userPopulate }
