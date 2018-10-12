67c5470d03777adabd1928f0224c0537c9339f8b
mohammad <khezri@outlook.com>
2018-10-11 21:29:46 -0500

    fixed minor Maggie's feedback

--- javascripts/components/chattyNav.js
+++ javascripts/components/chattyNav.js
@@ -1,17 +1,18 @@
 import { printToDom } from "../helpers/util.js";
 
-const users = ['Mohammad', 'Lance', 'Marco', 'Maggie'];
+const users = ["Mohammad", "Lance", "Marco", "Maggie"];
 
 const userPopulate = () => {
-  let domString = '';
+  let domString = "";
   domString += `<select class="selectpicker btn-success">`;
   domString += `<option class="FirstItemHide">Select User</option>`;
   for (let i = 0; i < users.length; i++) {
-    domString += `<option class="bg-light text-dark my-4" id="${users[i]}-user">${users[i]}</option>`;
-  };
+    domString += `<option class="bg-light text-dark my-4" id="${
+      users[i]
+    }-user">${users[i]}</option>`;
+  }
   domString += `</select>`;
-
   printToDom(domString, "users-select");
 };
 
-export { userPopulate }
+export { userPopulate };
