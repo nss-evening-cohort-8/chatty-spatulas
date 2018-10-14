69710fa3fcb47781a8b1c3673f69c423bfbe9b97
MCrank <marco_mcse@hotmail.com>
2018-10-13 19:41:05 -0500

    fixed missing message.id, cleaned up scroll function

--- javascripts/helpers/util.js
+++ javascripts/helpers/util.js
@@ -1,52 +1,53 @@
 const printToDom = (stringToPrint, elementId) => {
-	const selectedDiv = document.getElementById(elementId);
-	selectedDiv.innerHTML = stringToPrint;
+  const selectedDiv = document.getElementById(elementId);
+  selectedDiv.innerHTML = stringToPrint;
 };
 
-const msgInput = document.getElementById('message-input');
-const navUserSelect = document.getElementById('user-selected');
+const msgInput = document.getElementById("message-input");
+const navUserSelect = document.getElementById("user-selected");
 
 // Generate unique ID for our messages
 const getUniqueId = () => {
-	return Math.random().toString(36).substr(2, 16);
+  return Math.random()
+    .toString(36)
+    .substr(2, 16);
 };
 
 const getTime = () => {
-	return moment().format('h:mm:ss a');
+  return moment().format("h:mm:ss a");
 };
 
 const inputValidation = (messageError, userError) => {
-	if (messageError) {
-		msgInput.classList.add('is-invalid');
-		msgInput.setAttribute('placeholder', 'Message field must not be empty');
-	}
-	if (userError) {
-		navUserSelect.classList.replace('btn-success', 'btn-danger');
-	}
+  if (messageError) {
+    msgInput.classList.add("is-invalid");
+    msgInput.setAttribute("placeholder", "Message field must not be empty");
+  }
+  if (userError) {
+    navUserSelect.classList.replace("btn-success", "btn-danger");
+  }
 };
 
 const resetMessageInput = () => {
-	msgInput.value = '';
+  msgInput.value = "";
 };
 
 // pass in an event and the id of the button as a string
 const disableBtn = (e, buttonId) => {
-	const button = document.getElementById(buttonId);
-	if (e.target.id === buttonId) {
-		button.setAttribute('disabled', 'disabled');
-		button.classList.add('disabled');
-	}
+  const button = document.getElementById(buttonId);
+  if (e.target.id === buttonId) {
+    button.setAttribute("disabled", "disabled");
+    button.classList.add("disabled");
+  }
 };
 
 // pass id of button as a string
-const enableBtn = (buttonId) => {
-	document.getElementById(buttonId).removeAttribute('disabled');
-	document.getElementById(buttonId).classList.remove('disabled');
+const enableBtn = buttonId => {
+  document.getElementById(buttonId).removeAttribute("disabled");
+  document.getElementById(buttonId).classList.remove("disabled");
 };
 
-const setScrolDown = (divId) => {
-	const element = document.getElementById(divId).scrollIntoView();
-	// element.scrollTop = element.scrollHeight;
+const setScrolDown = divId => {
+  document.getElementById(divId).scrollIntoView();
 };
 
 export { printToDom, getTime, getUniqueId, inputValidation, resetMessageInput, enableBtn, disableBtn, setScrolDown };
