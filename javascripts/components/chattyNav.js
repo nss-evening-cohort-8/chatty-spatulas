bdce5562fbbc0aa07836ceaf5b3afcdf3df67e04
mohammad <khezri@outlook.com>
2018-10-14 12:40:01 -0500

    I had no change to this file

--- javascripts/components/chattyNav.js
+++ javascripts/components/chattyNav.js
@@ -1,15 +1,15 @@
-import { printToDom } from "../helpers/util.js";
+import { printToDom } from '../helpers/util.js';
 
-const users = ["Mohammad", "Lance", "Marco", "Maggie"];
+const users = [ 'Mohammad', 'Lance', 'Marco', 'Maggie' ];
 
 const userPopulate = () => {
-  let domString = "";
-  domString += `<option selected="selected">Select User</option>`;
-  for (let i = 0; i < users.length; i++) {
-    domString += `<option class="bg-light text-dark my-4" id="${users[i]}-user">${users[i]}</option>`;
-  }
-  domString += `</select>`;
-  printToDom(domString, "user-selected");
+	let domString = '';
+	domString += `<option selected="selected">Select User</option>`;
+	for (let i = 0; i < users.length; i++) {
+		domString += `<option class="bg-light text-dark my-4" id="${users[i]}-user">${users[i]}</option>`;
+	}
+	domString += `</select>`;
+	printToDom(domString, 'user-selected');
 };
 
 export { userPopulate };
