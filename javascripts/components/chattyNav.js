e1ca24e1f4466291ec3f281de9015250d9123514
mohammad <khezri@outlook.com>
2018-10-14 14:28:51 -0500

    Fixed navbar UI

--- javascripts/components/chattyNav.js
+++ javascripts/components/chattyNav.js
@@ -4,11 +4,10 @@ const users = [ 'Mohammad', 'Lance', 'Marco', 'Maggie' ];
 
 const userPopulate = () => {
 	let domString = '';
-	domString += `<option selected="selected">Select User</option>`;
+	domString += `<option selected id="none">Select User</option>`;
 	for (let i = 0; i < users.length; i++) {
-		domString += `<option class="bg-light text-dark my-4" id="${users[i]}-user">${users[i]}</option>`;
+		domString += `<option id="${users[i]}-user">${users[i]}</option>`;
 	}
-	domString += `</select>`;
 	printToDom(domString, 'user-selected');
 };
 
